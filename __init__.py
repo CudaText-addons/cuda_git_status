c06d6ca1c6cdde45e742ae1c26f60c16da39d146
Alexey <support@uvviewsoft.com>
2021-06-03 01:17:18 +0300

    refact

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from .git_manager import GitManager
 
 CELL_TAG_INFO = 20 #CudaText built-in value for last statusbar cell
 CELL_TAG = 100 #uniq value for all plugins adding cells via statusbar_proc()
+BAR_H = 'main'
 
 fn_config = os.path.join(app_path(APP_DIR_SETTINGS), 'cuda_git_status.ini')
 
@@ -14,18 +15,25 @@ class Command:
     def __init__(self):
 
         self.load_ops()
+        self.load_icon()
+
+    def init_bar_cell(self):
 
         #insert our cell before "info" cell
-        index = statusbar_proc('main', STATUSBAR_FIND_CELL, value=CELL_TAG_INFO)
-        if not index:
-            index = -1
-        statusbar_proc('main', STATUSBAR_ADD_CELL, index=index, tag=CELL_TAG)
-        statusbar_proc('main', STATUSBAR_SET_CELL_ALIGN, tag=CELL_TAG, value='C')
+        index = statusbar_proc(BAR_H, STATUSBAR_FIND_CELL, value=CELL_TAG_INFO)
+        if index is None:
+            return False
 
-        imglist = statusbar_proc('main', STATUSBAR_GET_IMAGELIST)
+        statusbar_proc(BAR_H, STATUSBAR_ADD_CELL, index=index, tag=CELL_TAG)
+        statusbar_proc(BAR_H, STATUSBAR_SET_CELL_ALIGN, tag=CELL_TAG, value='C')
+        return True
+
+    def load_icon(self):
+
+        imglist = statusbar_proc(BAR_H, STATUSBAR_GET_IMAGELIST)
         if not imglist:
             imglist = imagelist_proc(0, IMAGELIST_CREATE)
-            statusbar_proc('main', STATUSBAR_SET_IMAGELIST, value=imglist)
+            statusbar_proc(BAR_H, STATUSBAR_SET_IMAGELIST, value=imglist)
 
         fn_icon = os.path.join(
                     os.path.dirname(__file__),
@@ -53,25 +61,33 @@ class Command:
         if os.path.isfile(fn_config):
             file_open(fn_config)
 
-    def update(self):
+    def update(self, reason):
 
+        if not self.init_bar_cell():
+            #print('[Git Status] Statusbar not ready, '+reason)
+            return
+        #print('[Git Status] Statusbar ready, '+reason)
+        
         text = gitmanager.badge(ed.get_filename())
-        statusbar_proc('main', STATUSBAR_SET_CELL_TEXT, tag=CELL_TAG, value=text)
+        statusbar_proc(BAR_H, STATUSBAR_SET_CELL_TEXT, tag=CELL_TAG, value=text)
 
         #show icon?
         icon = self.icon_index if text else -1
-        statusbar_proc('main', STATUSBAR_SET_CELL_IMAGEINDEX, tag=CELL_TAG, value=icon)
+        statusbar_proc(BAR_H, STATUSBAR_SET_CELL_IMAGEINDEX, tag=CELL_TAG, value=icon)
 
         #show panel?
         size = self.cell_width if text else 0
-        statusbar_proc('main', STATUSBAR_SET_CELL_SIZE, tag=CELL_TAG, value=size)
+        statusbar_proc(BAR_H, STATUSBAR_SET_CELL_SIZE, tag=CELL_TAG, value=size)
 
 
     def on_tab_change(self, ed_self):
-        self.update()
+        self.update('on_tab_change')
 
     def on_open(self, ed_self):
-        self.update()
+        self.update('on_open')
 
     def on_save(self, ed_self):
-        self.update()
+        self.update('on_save')
+
+    def on_focus(self, ed_self):
+        self.update('on_focus')
